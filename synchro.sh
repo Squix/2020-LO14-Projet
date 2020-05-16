7ae47d5f63ae40fdb71819dd7a58580b4d79c4e1
Squix <squixnetwork@gmail.com>
2020-05-16 17:30:12 +0200

    Premiere fonction de la fonction de comparaison de fichiers, qui renvoit conflit s'il y a conflit ainsi qu'une description du conflit

--- synchro.sh
+++ synchro.sh
@@ -8,6 +8,42 @@ arbreB="tests/arbreB"
 #Affiche la hieracrhie des sous-repoertoires
 #ls -R tests/arbreB| grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
 #find tests/arbreA
+
+
+#fonction qui compare les fichiers
+compareFiles() {
+
+	local eq_arbreB="${1/$arbreA/$arbreB}"
+	local result=""
+
+	if [[ -f $eq_arbreB ]]; then
+		#le fichier existe et est un fichier
+		#echo "le fichier existe et est un fichier"
+
+		#teste si les 2 fichiers sont identiques (métadonnées)
+		if [[ "$(stat -c '%A%s%y' "$1")" == "$(stat -c '%A%s%y' "$eq_arbreB")" ]]; then
+			#echo "le fichier existe, est un fichier et est identique à celui de l'arbre B (meta)"
+			#TODO comparaison du contenu du fichier et verif avec le journal
+			result="ok"
+		else
+			#echo "le fichier existe, est un fichier mais ses meta sont différentes de celui de l'arbre B (meta)"
+			result="conflit;meta_diff"
+		fi
+
+	elif [[ -d $eq_arbreB ]]; then
+		#le fichier existe mais est un dossier
+		#echo "ERREUR - le fichier est un dossier !"
+		result="conflit;est_dossier"
+	else
+		#le fichier n'existe pas
+		#echo "ERREUR - fichier inexistant !"
+		result="conflit;inexistant"
+	fi
+
+	echo $result
+}
+
+
 #fonction de parcours récursive
 walk(){
 	local indent="${2:-0}"
@@ -17,6 +53,7 @@ walk(){
 	#si c'est un fichier on affiche son chemin
           if [[ -f "$entry" ]]; then
             printf "%*sF - %s\n" $indent '' "$entry"
+						compareFiles "$entry"
           #s'il sagit d'un dossier, on affiche et on descend dedans
           elif [[ -d "$entry" ]]; then
             printf "%*sD - %s\n" $indent '' "$entry"
@@ -24,9 +61,9 @@ walk(){
           fi
         done
 }
-walk tests/arbreA
+walk "$arbreA"
 #dpnne le contenu d'un seul répertoire, utile quand les fichiers ne sont pas dans le même ordre
-treeDirectory=$arbreA/*
-for i in "${treeDirectory[@]}"; do
-  echo $i
-done
+#treeDirectory=$arbreA/*
+#for i in "${treeDirectory[@]}"; do
+#  echo $i
+#done
