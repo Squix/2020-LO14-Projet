8e8dcaae0418a902f9cc04ce253216d9ece2a45e
gabriel-dcl <gabriel-dcl@users.noreply.github.com>
2020-05-16 17:51:14 +0100

    l'idée c'est que pendant l'opération du script, l'écriture se fasse sur log_temp, et que la lecture se fasse sur log_file. A la fin de l'exécution, log_temp se copie dans log_file, et soit supprimé par la suite.

--- synchro.sh
+++ synchro.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 arbreA="tests/arbreA"
 arbreB="tests/arbreB"
+adresseLog="log_file"
 # Projet final de LO14
 # Pour P20
 #Affiche la hieracrhie des sous-repoertoires
@@ -17,14 +18,27 @@ walk(){
 	#si c'est un fichier on affiche son chemin
           if [[ -f "$entry" ]]; then
             printf "%*sF - %s\n" $indent '' "$entry"
+						log_write $entry
           #s'il sagit d'un dossier, on affiche et on descend dedans
           elif [[ -d "$entry" ]]; then
             printf "%*sD - %s\n" $indent '' "$entry"
             walk "$entry" $((indent+4))
+						#log_write $entry
           fi
         done
+				#deqdq
 }
-walk tests/arbreA
+log_write()
+{	
+		#Si l'élément et un fichier, on ajoute f devant pour le représenter
+	if [[ -f "$1" ]]; then
+		printf "f %s \n" $1 >> log_temp
+				#Si l'élément et un dossier, on ajoute d devant pour le représenter
+	elif [[ -d "$1" ]]; then
+		printf "d %s \n" $1 >> log_temp
+ fi
+}
+walk $arbreA
 #dpnne le contenu d'un seul répertoire, utile quand les fichiers ne sont pas dans le même ordre
 treeDirectory=$arbreA/*
 for i in "${treeDirectory[@]}"; do
