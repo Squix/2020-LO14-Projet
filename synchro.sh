35cb02505b5e78d25c724b1e9225a43a57212233
gabriel-dcl <gabriel-dcl@users.noreply.github.com>
2020-05-16 15:31:29 +0100

    Update synchro.sh

--- synchro.sh
+++ synchro.sh
@@ -1,31 +1,20 @@
 #!/usr/bin/env bash
-
 arbreA="tests/arbreA"
 arbreB="tests/arbreB"
-
 # Projet final de LO14
 # Pour P20
-
 #Affiche la hieracrhie des sous-repoertoires
-
 #ls -R tests/arbreA | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
-
 #Affiche la hieracrhie des sous-repoertoires
-
 #ls -R tests/arbreB| grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
-
 #find tests/arbreA
-
 #fonction de parcours récursive
-walk() {
-        local indent="${2:-0}"
-        #echo "$1"/*
-
-        #pour chaque élément du répertoire
-        for entry in "$1"/*; do
-
-
-          #si c'est un fichier, on affiche son chemin
+walk(){
+	local indent="${2:-0}"
+	#echo "$1"/*
+	#pour chaque élément du répertoire
+	for entry in "$1"/*; do
+	#si c'est un fichier on affiche son chemin
           if [[ -f "$entry" ]]; then
             printf "%*sF - %s\n" $indent '' "$entry"
           #s'il sagit d'un dossier, on affiche et on descend dedans
@@ -33,11 +22,9 @@ walk() {
             printf "%*sD - %s\n" $indent '' "$entry"
             walk "$entry" $((indent+4))
           fi
-
         done
 }
-#walk tests/arbreA
-
+walk tests/arbreA
 #dpnne le contenu d'un seul répertoire, utile quand les fichiers ne sont pas dans le même ordre
 treeDirectory=$arbreA/*
 for i in "${treeDirectory[@]}"; do
